abc2c5dbb49932d81d3caaa8c4874868c06635b1
VladislavSidorovich <sidinfo640@gmail.com>
2025-02-03 21:49:31 +0300

    feat: Update CustomSliderСlosedSection.tsx

--- components/CustomSliderСlosedSection.tsx
+++ components/CustomSliderСlosedSection.tsx
@@ -201,14 +201,14 @@ function Index() {
             <SwiperSlide
               key={index}
               className={
-                index === 1 ||
-                index === 2 ||
                 index === 3 ||
                 index === 4 ||
                 index === 5 ||
                 index === 6 ||
                 index === 7 ||
-                index === 8
+                index === 8 ||
+                index === 9 ||
+                index === 10
                   ? "hidden-slide"
                   : ""
               }
