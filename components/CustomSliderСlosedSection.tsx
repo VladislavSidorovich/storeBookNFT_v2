0c7362f29c85e6fafe0a593f504a3da921fff5b0
VladislavSidorovich <sidinfo640@gmail.com>
2025-02-24 18:01:09 +0300

    feat; Update CustomSliderСlosedSection.tsx

--- components/CustomSliderСlosedSection.tsx
+++ components/CustomSliderСlosedSection.tsx
@@ -206,13 +206,15 @@ function Index() {
                 index === 3 ||
                 index === 2 ||
                 index === 5 ||
-                index === 6 ||
+                index === 4 ||
                 index === 7 ||
                 index === 8 ||
                 index === 9 ||
                 index === 10 || 
                 index === 11 || 
-                index === 12
+                index === 12 || 
+                index === 13 || 
+                index === 14
                   ? "hidden-slide"
                   : ""
               }
